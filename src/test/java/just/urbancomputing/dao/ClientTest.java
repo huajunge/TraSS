12738924c3dfd9fa02379c876ffded7b84bf5825
hehuajun3 <hehuajun3@jd.com>
2021-09-15 20:19:22 +0800

    Initial commit

---
+++ src/test/java/just/urbancomputing/dao/ClientTest.java
@@ -0,0 +1,13 @@
+package just.urbancomputing.dao;
+
+import junit.framework.TestCase;
+
+public class ClientTest extends TestCase {
+
+    public void testKnnQuery() {
+
+    }
+
+    public void testQuery() {
+    }
+}
